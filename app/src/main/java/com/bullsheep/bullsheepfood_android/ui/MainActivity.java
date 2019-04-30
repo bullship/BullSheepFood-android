9954c6aaf0c5cdaad6869410a92823d798327c88
Nazarii Moshenskyi <nazarii.moshenskyi@globallogic.com>
2019-04-30 13:12:37 +0300

    Added basic stats fragment layout

--- app/src/main/java/com/bullsheep/bullsheepfood_android/ui/MainActivity.java
+++ app/src/main/java/com/bullsheep/bullsheepfood_android/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.bullsheep.bullsheepfood_android;
+package com.bullsheep.bullsheepfood_android.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.bullsheep.bullsheepfood_android.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, new StatsFragment())
+                .commit();
     }
 }
