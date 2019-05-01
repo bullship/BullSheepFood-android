36c17404262b9d3e5ce285ea5970794432f76aae
Artem Bashtovyi <artembashtovyi@gmail.com>
2019-05-01 15:58:56 +0300

    Scan nutritions added

--- app/src/main/java/com/bullsheep/bullsheepfood_android/ui/AddFoodFragment.java
+++ app/src/main/java/com/bullsheep/bullsheepfood_android/ui/AddFoodFragment.java
@@ -1,20 +1,33 @@
 package com.bullsheep.bullsheepfood_android.ui;
 
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bullsheep.bullsheepfood_android.R;
 import com.bullsheep.bullsheepfood_android.dto.ProductDTO;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import static android.app.Activity.RESULT_OK;
 
 
 /**
@@ -23,6 +36,7 @@ import androidx.fragment.app.Fragment;
 public class AddFoodFragment extends Fragment {
 
     private static final String TAG = AddFoodFragment.class.getName();
+    private static final int REQUEST_IMAGE_CAPTURE_CODE = 1024;
 
     private EditText nameEt;
     private EditText proteinsEt;
@@ -30,6 +44,7 @@ public class AddFoodFragment extends Fragment {
     private EditText carbsEt;
     private EditText kCalEt;
     private Button submitBtn;
+    private ImageView scanIv;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
@@ -50,6 +65,7 @@ public class AddFoodFragment extends Fragment {
     }
 
     private void initViews(View view) {
+        scanIv = view.findViewById(R.id.fragment_add_food_scan_iv);
         nameEt = view.findViewById(R.id.add_food_name_et);
         proteinsEt = view.findViewById(R.id.add_food_proteins_et);
         fatsEt = view.findViewById(R.id.add_food_fats_et);
@@ -60,6 +76,27 @@ public class AddFoodFragment extends Fragment {
 
     private void initClickListeners() {
         submitBtn.setOnClickListener((view) -> submitData());
+        scanIv.setOnClickListener((view) -> requestTakePictureActivity());
+    }
+
+
+    private void requestTakePictureActivity() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE_CODE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE_CODE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            if (extras != null) {
+                Bitmap bitmap = (Bitmap) extras.get("data");
+                runTextRecognition(bitmap);
+            }
+        }
     }
 
     private void submitData() {
@@ -72,6 +109,61 @@ public class AddFoodFragment extends Fragment {
         Log.i(TAG, "submitData: " + product);
         // TODO: 30.04.2019 send to back
         getActivity().getSupportFragmentManager().popBackStack();
+    }
+
+
+    private void runTextRecognition(Bitmap imageBitmap) {
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(imageBitmap);
+        FirebaseVisionTextRecognizer recognizer = FirebaseVision.getInstance()
+                .getOnDeviceTextRecognizer();
+        scanIv.setEnabled(false);
+        recognizer.processImage(image)
+                .addOnSuccessListener(
+                        texts -> {
+                            scanIv.setEnabled(true);
+                            processTextRecognitionResult(texts);
+                        })
+                .addOnFailureListener(
+                        e -> {
+                            // Task failed with an exception
+                            Toast.makeText(this.getContext(),
+                                    "Fail to process", Toast.LENGTH_LONG).show();
+                            scanIv.setEnabled(true);
+                            e.printStackTrace();
+                        });
+    }
+
+    private void processTextRecognitionResult(FirebaseVisionText texts) {
+        List<FirebaseVisionText.TextBlock> blocks = texts.getTextBlocks();
+        StringBuilder sb = new StringBuilder("Result : ");
+        if (blocks.size() == 0) {
+            String empty = "No text found";
+            Toast.makeText(this.getContext(), empty, Toast.LENGTH_LONG).show();
+            sb.append(empty);
+            return;
+        }
+        for (int i = 0; i < blocks.size(); i++) {
+            List<FirebaseVisionText.Line> lines = blocks.get(i).getLines();
+            for (int j = 0; j < lines.size(); j++) {
+                sb.append("\n");
+                List<FirebaseVisionText.Element> elements = lines.get(j).getElements();
+                for (int k = 0; k < elements.size(); k++) {
+                    FirebaseVisionText.Element element = elements.get(k);
+                    String text = element.getText();
+                    Log.d("ELEMENT: ", text);
+                    sb.append(text);
+                }
+            }
+        }
+        Log.d(TAG, sb.toString());
+        show(sb.toString());
+    }
+
+    private void show(String text) {
+        proteinsEt.setText(ParseUtil.parseProtein(text));
+        kCalEt.setText(ParseUtil.parseCalories(text));
+        carbsEt.setText(ParseUtil.parseCarb(text));
+        fatsEt.setText(ParseUtil.parseFat(text));
     }
 
     private ProductDTO readData() {
